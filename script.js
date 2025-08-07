18ac55469642985326052072f0dd4b4015500216
nclangeek <nclangeek@mac.com>
2025-08-06 19:07:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,20 @@ document.addEventListener('DOMContentLoaded', function() {
             snippet: "In that moment of performing for the first time on stage I was like I knew that my music was bigger than myself this one clip just went viral of Gloomy Sunday and everything really like changed in a way that I never expected."
         },		
 		{
+            title: "Angelina Jordan Blog Entries by Ken Chawkin",
+            source: "The Uncarved Blog",
+            link: "https://theuncarvedblog.com/tag/angelina-jordan/",
+            date: "December 20, 2023",
+            snippet: "On December 8, 2023 Angelina Jordan Official/Republic Records/UMG released her new 5-song Christmas album, Driving Home for Christmas."
+        },		
+		{
+            title: "Angelina Jordan Releases New EP, 'Old Enough'",
+            source: "Prelude Press",
+            link: "https://preludepress.com/news/2023/07/21/angelina-jordan-old-enough/",
+            date: "July 21, 2023",
+            snippet: "Acclaimed songstress and vocal dynamo, Angelina Jordan has unveiled her brand-new EP, 'Old Enough' today via Republic Records.  To celebrate the project’s release, she also dropped the music video for the single 'Now I’m The Fool.'"
+        },		
+		{
             title: "Angelina Teases Her Forthcoming Debut Album | UNFO 2022 Red Carpet with Steven Lim",
             source: "Character Media",
             link: "https://www.smoothradio.com/artists/queen/girl-sings-bohemian-rhapsody-americas-got-talent-video/",
@@ -28,11 +42,32 @@ document.addEventListener('DOMContentLoaded', function() {
             snippet: "“America’s Got Talent” alum and viral sensation Angelina Jordan shares what’s coming up next for her and dishes on her new album."
         },
 		{
+            title: "13-yr-old's completely unique 'Bohemian Rhapsody' rendition was so great it even wowed Queen",
+            source: "Upworthy",
+            link: "https://www.upworthy.com/angelina-jordan-bohemian-rhapsody-ex1",
+            date: "Unknown-2022?",
+            snippet: "At Upworthy, we've shared a lot of memorable 'America's Got Talent' auditions, from physics-defying dance performances to jaw-dropping magic acts to heart-wrenching singer-songwriter stories. Now we're adding Angelina Jordan's 'AGT: The Champions' audition to the list because wow."
+        },	
+		{
             title: "Best TV audition of all time? Girl, 13, sings staggering original soul version of Queen's 'Bohemian Rhapsody'",
             source: "Smooth Radio Website",
             link: "https://www.smoothradio.com/artists/queen/girl-sings-bohemian-rhapsody-americas-got-talent-video/",
             date: "September 23, 2022",
             snippet: "A video of 13-year-old Angelina Jordan singing on America's Got Talent in January 2020 has been seen over 24 million times and dubbed 'the greatest audition of all time'."
+        },
+		{
+            title: "NRK TV Interview at Kongsberg Jazz Festival July 2022",
+            source: "NRK TV - Kongsberg",
+            link: "https://www.youtube.com/watch?v=u8zSWTlCltA",
+            date: "July 7, 2022",
+            snippet: "Angelina's Interview by NRK TV at Kongsberg July 7, 2022 (Subtitled in many languages) Aired on NRK TV in Norway August 6, 2022"
+        },
+		{
+            title: "The Story of Angelina Jordan|Beyond Got Talent",
+            source: "Epic Top Trending YouTube Channel",
+            link: "https://www.youtube.com/watch?v=kOZda1fDw6o",
+            date: "February 19, 2022",
+            snippet: "Angelina Jordan is a 16-year-old singer who became a viral sensation when she auditioned for the 2014 season of Norway's Got Talent when she was just 8 years old."
         },
         {
             title: "Angelina Jordan on joining the industry young, 'Million Miles', and more",
@@ -48,7 +83,14 @@ document.addEventListener('DOMContentLoaded', function() {
             date: "February 4, 2021",
             snippet: "Norwegian singer Angelina Jordan — who went viral with her giant vocals in 2014 — is back with her new single 'Million Miles'."
         },
-        {
+		{
+            title: "Angelina Jordan interview Norwegian TV2",
+            source: "Norwegian TV2",
+            link: "https://www.youtube.com/watch?v=ni_bQ74Ga4A",
+            date: "February 17, 2020",
+            snippet: "Interview on Norwegian TV2 about AGT-Champions finale 18/2-2020"
+        },	
+		{
             title: "11-year-old Angelina Jordan performs 'It's Now or Never'",
             source: "Elvis Australia Official Elvis Presley Fan Club",
             link: "https://www.elvis.com.au/presley/11-year-old-angelina-jordan-performs-elvis-it-s-now-or-never.shtml",
